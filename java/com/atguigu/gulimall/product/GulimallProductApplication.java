69d5009be6a11a122e28a4cf2e1b0aedadb4de15
junzhongLiu <junsunnyliu@163.com>
2023-08-02 11:27:57 +0800

    测试

--- java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
 /**
  * 1、整合MyBatis-Plus
  *      1）、导入依赖
